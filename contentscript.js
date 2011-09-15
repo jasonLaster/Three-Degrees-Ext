64b741c6132db9eeeb7e3dafcb52921d3c0cdfc9
Jason Laster <Jason.laster.11@gmail.com>
2011-09-14 23:10:04 -0700

    #1 remove data from other pagelets

--- contentscript.js
+++ contentscript.js
@@ -18,12 +18,15 @@ $(document).ready(function(){
 
 function parseFbSearch(data) {
 
+	// CLEAN DATA
+	var clean_data = /.+big_pipe.+pagelet_search_resul.+/.exec(data)[0]
+
 	// GET URLS	
-	var urls = data.match(/https:\\\/\\\/www.facebook.com\\\/\S+ /g)
-	
+	var urls = clean_data.match(/https:\\\/\\\/www.facebook.com\\\/\S+ /g)
+
 	// CLEAN URLS
 	urls = _(urls).map(function(url){ return url.replace(/.+com\\\//,'').replace(/\\\"/,'').trim();});
-	
+
 	// GET UIDS	
 	var uids = 
 		_(urls).chain()
@@ -31,18 +34,17 @@ function parseFbSearch(data) {
 		.map(function(url){ return /\d+/.exec(url)[0]})
 		.uniq()
 		.value()
-		
+	
 	// GET USER_NAMES
 	var user_names = _(urls).reject(function(url){return url.match(/(php|\/)/)})
 	var user_names = _.uniq(user_names)
-	
+
 	// LIST
 	var list = []
 	list.push(uids)
 	list.push(user_names)
 	list = _.flatten(list)
-	list = _.sortBy(list, function(i){ return data.search(i); })
-	
+	list = _.sortBy(list, function(i){ return clean_data.search(i); })
 	
 }
 
